450a0e684d4e3769297baf8af04855d0d018564a
martinotijn <78542188+martinotijn@users.noreply.github.com>
2022-03-23 10:46:05 +0100

    Add POS tag

--- brown.py
+++ brown.py
@@ -1,28 +1,46 @@
 import nltk
 from nltk.corpus import brown
 from nltk import Counter
-
+import random
 mystery = {}
 romance = {}
 
-
+random.seed(1)
 cfd = nltk.ConditionalFreqDist( (genre, word) for genre in brown.categories() for word in brown.words(categories=genre))
 genre_word = [(genre, word) for genre in ['mystery', 'romance'] for word in brown.words(categories=genre)]
 genres = ['romance', 'mystery']
 
-modals = ["I", "you" ,"he", "she", "it", "we", "they", "them", "us", "him", "her", "his", "hers", "its", "theirs", "our", "your"]
-wordcounts = Counter(brown.words(categories='romance'))
-for i in modals:
-    
-    romance[i] = wordcounts[i]/len(wordcounts)
-wordcounts = Counter(brown.words(categories='mystery'))
+modals = ['i', 'me', 'you', 'he', 'him', 'she', 'her', 'it', 'we', 'us', 'they', 'them', 'one','I', 'Me', 'You', 'He', 'Him', 'She', 'Her', 'It', 'We', 'Us', 'They', 'Them', 'One']
+sentences = brown.tagged_sents(tagset='universal', categories='romance')
 
-for i in modals:
-    
-    mystery[i] = wordcounts[i]/len(wordcounts)
-    
-print(mystery)
-print(romance)
+print(len(sentences))
+mystery = brown.sents(categories='mystery')
+sentences_romance = []
+sentences_mystery = []
+for i in sentences:
+    if len(i) > 8 and len(i) < 10:
+        sentences_romance.append(i)
+        
+print(len(sentences_romance))
 
+for i in mystery:
+    if len(i) > 6 and len(i) < 10:
+        sentences_mystery.append(i)
+        
+print(len(sentences_mystery))
 
-cfd.tabulate(conditions=genres, samples=modals)
+random_romance = random.sample(sentences_romance, 100)
+random_mystery = random.sample(sentences_mystery, 100)
+
+count_romance = []
+count_mystery = []
+for i in random_romance:
+    count_romance.append(sum(el in i for el in modals))
+
+for i in random_mystery:
+    print(i)
+    count_mystery.append(sum(el in i for el in modals))
+
+print(sentences[0])
+
+print(count_mystery)
